2b45eb40fb500b36b61e56100dd7812c6356e771
ahmady016 <ahmady09@gmail.com>
2019-02-25 06:55:43 +0200

    Add packeges, common components/modules and adjust basic app structure

---
+++ src/views/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './app.css';
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#!">
+          <img src="https://nckenya.com/wp-content/uploads/books.png" alt="books App"/>
+            The Books Bank
+          </a>
+        </nav>
+      </header>
+      <div className="container">
+      </div>
+    </>
+  );
+}
